da3a680db6e3dfc8d4770058225cde203bc1e332
sakssgod <pika.goalfa30@gmail.com>
2024-11-12 01:34:31 +0000

    写了主页的一个小demo，明天在完善icon

---
+++ scripts/HomePage.js
@@ -0,0 +1,26 @@
+const body = document.querySelector('body'),
+      shell = body.querySelector('.shell'),
+      toggle = body.querySelector('.toggle'),
+      searchBtn = body.querySelector('.search-box'),
+      modeSwitch = body.querySelector('.toggle-switch'),
+      modeText = body.querySelector('.mode-text');
+
+// 切换侧边栏宽度
+toggle.addEventListener("click", () => {
+    shell.classList.toggle("close");
+});
+
+// 点击搜索框时打开侧边栏
+searchBtn.addEventListener("click", () => {
+    shell.classList.remove("close");
+});
+
+// 切换主题模式
+modeSwitch.addEventListener("click", () => {
+    body.classList.toggle("dark");
+    if (body.classList.contains("dark")) {
+        modeText.innerText = "白日模式";
+    } else {
+        modeText.innerText = "夜间模式";
+    }
+});
