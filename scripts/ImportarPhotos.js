76868bb91065964fafbfc350c205c5cf3cad092e
sakssgod <pika.goalfa30@gmail.com>
2024-11-25 02:02:03 +0000

    312312312

--- scripts/ImportarPhotos.js
+++ scripts/ImportarPhotos.js
@@ -16,6 +16,7 @@ document.addEventListener('click', function() {
 // 获取 file-explorer 的引用
 const fileExplorer = document.getElementById('fileExplorer');
 const warningAddContainer = document.getElementById('warningAddContainer');
+const overlay = document.getElementById('overlay');
 
 // 为 open 按钮添加事件监听器
 document.querySelector('.bottom-open-cancele .bottom-button:first-child').addEventListener('click', function () {
@@ -27,26 +28,32 @@ document.querySelector('.bottom-open-cancele .bottom-button:first-child').addEve
     warningAddContainer.style.display = 'none';
 });
 
+
 // 为 cancele 按钮添加事件监听器
 document.querySelector('.bottom-open-cancele .bottom-button:last-child').addEventListener('click', function () {
     fileExplorer.style.display = 'none'; // 隐藏 file-explorer
 });
 
-function openIframe(url) {
-    const iframeContainer = document.getElementById('iframeContainer');
-    const criteriosIframe = document.getElementById('criteriosIframe');
-    if (iframeContainer && criteriosIframe) {
-        criteriosIframe.src = url; // 设置 iframe 的链接
-        iframeContainer.style.display = 'block'; // 显示 iframe 容器
-    }
+
+overlay.classList.add('overlay');
+document.body.appendChild(overlay);
+
+// 打开文件浏览器并显示遮罩
+function openFileExplorer() {
+    fileExplorer.style.display = 'flex'; // 显示文件浏览器
+    overlay.style.display = 'block'; // 显示遮罩
 }
 
-function closeIframe() {
-    const iframeContainer = document.getElementById('iframeContainer');
-    const criteriosIframe = document.getElementById('criteriosIframe');
-    if (iframeContainer && criteriosIframe) {
-        criteriosIframe.src = ''; // 清空 iframe 的链接
-        iframeContainer.style.display = 'none'; // 隐藏 iframe 容器
-    }
+// 关闭文件浏览器并隐藏遮罩
+function closeFileExplorer() {
+    fileExplorer.style.display = 'none'; // 隐藏文件浏览器
+    overlay.style.display = 'none'; // 隐藏遮罩
 }
 
+// 阻止点击文件浏览器内容时关闭
+fileExplorer.addEventListener('click', (event) => {
+    event.stopPropagation();
+});
+
+// 绑定遮罩点击事件
+overlay.addEventListener('click', closeFileExplorer);
